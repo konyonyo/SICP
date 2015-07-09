b3ad1969909b2df1ade20215f4ddf734b1afc4f0
Yuichiro <yuichiro.utsumi@gmail.com>
2015-07-09 22:17:51 +0900

    対数的ステップ数のフィボナッチ数列計算

--- problem1.scm
+++ problem1.scm
@@ -301,3 +301,25 @@
    [(= b 0) n]
    [(even? b) (*-iter (double a) (halve b) n)]
    [else (*-iter a (- b 1) (+ n a))]))
+
+; 1.19
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q n)
+  (cond
+   [(= n 0) b]
+   [(even? n) (fib-iter a
+			b
+			(+ (* p p) (* q q))
+			(+ (* 2 p q) (* q q))
+			(/ n 2))]
+   [else (fib-iter (+ (* b q) (* a q) (* a p))
+		   (+ (* b p) (* a q))
+		   p
+		   q
+		   (- n 1))]))
+
+
+
+
