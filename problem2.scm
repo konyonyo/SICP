62ac780cab7c5bc063d49a8e7be4e5ddecd9368a
Yuichiro <yuichiro.utsumi@gmail.com>
2015-08-29 01:04:14 +0900

    クォート

--- problem2.scm
+++ problem2.scm
@@ -948,3 +948,33 @@
   (let ((combine4 (square-of-four flip-vert rotate180
 				  identity  flip-horiz)))
     (combine4 (corner-split painter n))))
+
+;; 2.53
+(list 'a 'b 'c)
+;; => (a b c)
+(list (list 'george))
+;; => ((george))
+(cdr '((x1 x2) (y1 y2)))
+;; => ((y1 y2))
+(cadr '((x1 x2) (y1 y2)))
+;; => (y1 y2)
+(pair? (car '(a short list)))
+;; => #f
+(define (memq item x)
+  (cond [(null? x) #f]
+	[(eq? item (car x)) x]
+	[else (memq item (cdr x))]))
+(memq 'red '((red shoes) (blue socks)))
+;; => #f
+(memq 'red '(red shoes blue socks))
+;; => (red shoes blue socks)
+
+;; 2.54
+(define (equal? a b)
+  (cond [(and (not (pair? a)) (not (pair? b))) (eq? a b)]
+	[(and (pair? a) (pair? b)) (and (equal? (car a) (car b))
+					(equal? (cdr a) (cdr b)))]
+	[else #f]))
+
+;; 2.55
+;; abracadabraÇÕ'(quote abracadabra)Ç…ìôÇµÇ¢ÇΩÇﬂÅBx
